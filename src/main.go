082dc9094f588f6d4aadf8aa6573e7af46595a32
Joseph-Joy-Ender <144816729+Joseph-Joy-Ender@users.noreply.github.com>
2024-03-04 08:42:03 +0100

    diary

--- src/main.go
+++ src/main.go
@@ -1,5 +1,43 @@
 package main
 
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
+
+func homePage(context *gin.Context) {
+	context.JSON(200, gin.H{
+		"message": "Hello World",
+	})
+}
+
+func postHomePage(context *gin.Context) {
+	context.JSON(200, gin.H{
+		"message": "Post Home page",
+	})
+}
 func main() {
+	fmt.Println("Hello World")
+
+	r := gin.Default()
+	r.GET("/", homePage)
+	r.POST("/", postHomePage)
+	r.GET("/query", queryString)
+
+	err := r.Run()
+	if err != nil {
+		return
+	}
+
+}
+
+func queryString(context *gin.Context) {
+	name := context.Query("name")
+	age := context.Query("age")
+
+	context.JSON(200, gin.H{
+		"name": name,
+		"age":  age,
+	})
 
 }
